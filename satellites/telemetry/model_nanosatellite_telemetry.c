b41c063a1c8cb9749a031323e8f4b2db7e950d68
brianoure <61065227+brianoure@users.noreply.github.com>
2023-07-26 12:03:55 +0300

    Update model_nanosatellite_telemetry.c

--- satellites/telemetry/model_nanosatellite_telemetry.c
+++ satellites/telemetry/model_nanosatellite_telemetry.c
@@ -1,22 +1,26 @@
-unsigned int * get_nanosatellite_telemetry(int solar_panel,
-                                           int battery_charge,
-                                           int battery_temperature,
-                                           long position_latitude,
-                                           long position_longitude,
-                                           unsigned int position_altitude,
-                                           long system_time,
-                                           char payload_status,
-                                           int payload_temperature,
-                                           char obc_status,
-                                           int memory_remaining,
-                                           int last_uplink_command,
-                                           int last_uplink_command_parameters,
-                                           int obc_temperature,
-                                           char structure_status,
-                                           int structure_temperature,
-                                           char eps_status,
-                                           int eps_temperature,
-                                           char attitude_determination_status ){//get_nanosatellite_telemetry
+void get_nanosatellite_telemetry(long preamble,
+                                 int solar_panel_voltage,
+                                 int battery_charge,
+                                 int battery_temperature,
+                                 long position_latitude,
+                                 long position_longitude,
+                                 unsigned int position_altitude,
+                                 long system_time,
+                                 char payload_status,
+                                 int payload_temperature,
+                                 char obc_status,
+                                 int memory_remaining_percentage,
+                                 long last_uplink_command,
+                                 long last_uplink_command_parameters,
+                                 int obc_temperature,
+                                 char structure_status,
+                                 int structure_temperature,
+                                 char eps_status,
+                                 int eps_temperature,
+                                 char attitude_determination_status 
+                                 int redundancy_check ){//get_nanosatellite_telemetry
+long_size=sizeof(long);
+int_size=sizeof(int);
 unsigned int first; unsigned int second; unsigned int third; unsigned int fourth; unsigned int fifth; unsigned int sixth;
 first = ( (solar_panel)|
           (battery_charge)|
@@ -41,5 +45,5 @@ sixth = ( (structure_temperature)|
           (attitude_determination_status) );
 unsigned int rslt[4];
 rslt = [first,second,third,fourth];
-return rslt;
+return 0;
 }//get_nanosatellite_telemetry
