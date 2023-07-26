e462ead85308e84639ad68c694d0ec3ad10ceb4b
brianoure <61065227+brianoure@users.noreply.github.com>
2023-07-26 14:11:46 +0300

    Update model_nanosatellite_telemetry.c

--- satellites/telemetry/model_nanosatellite_telemetry.c
+++ satellites/telemetry/model_nanosatellite_telemetry.c
@@ -1,55 +1,126 @@
-preamble is 'unsigned long' type
-- solar_panel_voltage is 'unsigned int' type
-- battery_charge is 'unsigned int' type
-- battery_temperature is 'int' type
-- position_latitude is 'unsigned long' type
-- position_longitude is 'unsigned long' type
-- position_altitude is 'unsigned int' type
-- system_time is 'unsigned long' type
-- payload_status is 'unsigned char' type
-- payload_temperature is 'int' type
-- obc_status is 'unsigned char' type
-- int memory_remaining_percentage is 'unsigned int' type
-- last_uplink_command is 'unsigned long' type
-- last_uplink_command_parameter1 is 'unsigned long' type
-- last_uplink_command_parameter2 is 'unsigned long' type
-- obc_temperature is 'int' type
-- structure_status is 'unsigned char' type
-- structure_temperature is 'int' type
-- eps_status  is 'unsigned char' type
-- eps_temperature is 'int' type
-- attitude_determination_status is 'unsigned char' type
-- communication_status is 'unsigned char' type
-- redundancy_check is 'unsigned int' type
 /*
-* Should be 32-bit machine friendly
+* Code to be executed in the nanosatellite
+* Should work in all 32-bit machines - avoided using 'long' type
 */
-void get_nanosatellite_telemetry(unsigned short preamble1,
-                                 unsigned short preamble2,
-                                 unsigned short preamble3,
-                                 unsigned short preamble4,
-                                 int solar_panel_voltage,
-                                 int battery_charge,
-                                 int battery_temperature,
-                                 long position_latitude,
-                                 long position_longitude,
-                                 unsigned int position_altitude,
-                                 long system_time,
-                                 char payload_status,
-                                 int payload_temperature,
-                                 char obc_status,
-                                 int memory_remaining_percentage,
-                                 long last_uplink_command,
-                                 long last_uplink_command_parameters,
-                                 int obc_temperature,
-                                 char structure_status,
-                                 int structure_temperature,
-                                 char eps_status,
-                                 int eps_temperature,
-                                 char attitude_determination_status, 
-                                 int redundancy_check ){//get_nanosatellite_telemetry
-long_size=sizeof(long);
-int_size=sizeof(int);
+int* generate_nanosatellite_telemetry_frame(
+unsigned short preamble1,
+unsigned short preamble2,
+unsigned short preamble3,
+unsigned short preamble4,
+unsigned int solar_panel_voltage,
+unsigned int battery_charge,
+unsigned int battery_temperature,
+unsigned short position_latitude1,
+unsigned short position_latitude2,
+unsigned short position_longitude1,
+unsigned short position_longitude2,
+unsigned char position_altitude1,
+unsigned char position_altitude2,
+unsigned char position_altitude3,
+unsigned short system_time1,
+unsigned short system_time2,
+unsigned char payload_status,
+unsigned int payload_temperature,
+unsigned char obc_status,
+unsigned int memory_remaining_percentage,
+unsigned short last_uplink_command1,
+unsigned short last_uplink_command2,
+unsigned short last_uplink_command_parameters1,
+unsigned short last_uplink_command_parameters2,
+unsigned int obc_temperature,
+unsigned char structure_status,
+unsigned int structure_temperature,
+unsigned char eps_status,
+unsigned int eps_temperature,
+unsigned char attitude_determination_status, 
+unsigned char redundancy_check
+) {//get_nanosatellite_telemetry
 
-return 0;
+int result_pointer[];
+short_size=(int) sizeof(short)
+int_size  =(int) sizeof(int)
+char_size =(int) sizeof(char)
+
+//short: preamble1 preamble2 preamble3 preamble4
+for(int x=0;x<short_size;x++){
+}/* short loop */
+for(int x=0;x<short_size;x++){
+}/* short loop */
+for(int x=0;x<short_size;x++){
+}/* short loop */
+for(int x=0;x<short_size;x++){
+}/* short loop */
+
+//int: solar_panel_voltage, battery_charge, battery_temperature
+for(int x=0;x<int_size;x++){}/* int loop */
+for(int x=0;x<int_size;x++){}/* int loop */
+for(int x=0;x<int_size;x++){}/* int loop */
+
+//short: postion_latitude1, postion_latitude2, postion_longitude1, postion_longitude2
+for(int x=0;x<short_size;x++){
+}/* short loop */
+for(int x=0;x<short_size;x++){
+}/* short loop */
+for(int x=0;x<short_size;x++){
+}/* short loop */
+for(int x=0;x<short_size;x++){
+}/* short loop */
+
+//int: position_altitude1, position_altitude2, position_altitude3
+for(int x=0;x<char_size;x++){}/* char loop */
+for(int x=0;x<char_size;x++){}/* char loop */
+for(int x=0;x<char_size;x++){}/* char loop */
+
+//short: system_time1, system_time2
+for(int x=0;x<short_size;x++){
+}/* short loop */
+for(int x=0;x<short_size;x++){
+}/* short loop */
+
+//char: payload_status
+for(int x=0;x<char_size;x++){}/* char loop */
+
+//int: payload_temperature
+for(int x=0;x<int_size;x++){}/* int loop */
+
+//char: obc_status
+for(int x=0;x<char_size;x++){}/* char loop */
+
+//int memory_remaining_percentage
+for(int x=0;x<int_size;x++){}/* int loop */
+
+//short: last_uplink_command1 , last_uplink_command2
+for(int x=0;x<short_size;x++){
+}/* short loop */
+for(int x=0;x<short_size;x++){
+}/* short loop */
+
+//short: last_uplink_command_parameters1, last_uplink_command_parameters2
+for(int x=0;x<short_size;x++){
+}/* short loop */
+for(int x=0;x<short_size;x++){
+}/* short loop */
+
+//int: obc_temperature
+for(int x=0;x<int_size;x++){}/* int loop */
+
+//char: structure_status
+for(int x=0;x<char_size;x++){}/* char loop */
+
+//int: structure_temperature
+for(int x=0;x<int_size;x++){}/* int loop */
+
+//char: eps_status
+for(int x=0;x<char_size;x++){}/* char loop */
+
+//int: eps_temperature
+for(int x=0;x<int_size;x++){}/* int loop */
+
+//char: attitude_determination_status
+for(int x=0;x<char_size;x++){}/* char loop */
+
+//char: redundancy_check
+for(int x=0;x<char_size;x++){}/* char loop */
+
+return result_pointer;
 }//get_nanosatellite_telemetry
