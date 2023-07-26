5267aeb2bc07040a999eba0e941c20efc98ee5c0
brianoure <61065227+brianoure@users.noreply.github.com>
2023-07-26 12:19:10 +0300

    Update model_nanosatellite_telemetry.c

--- satellites/telemetry/model_nanosatellite_telemetry.c
+++ satellites/telemetry/model_nanosatellite_telemetry.c
@@ -1,4 +1,33 @@
-void get_nanosatellite_telemetry(long preamble,
+preamble is 'unsigned long' type
+- solar_panel_voltage is 'unsigned int' type
+- battery_charge is 'unsigned int' type
+- battery_temperature is 'int' type
+- position_latitude is 'unsigned long' type
+- position_longitude is 'unsigned long' type
+- position_altitude is 'unsigned int' type
+- system_time is 'unsigned long' type
+- payload_status is 'unsigned char' type
+- payload_temperature is 'int' type
+- obc_status is 'unsigned char' type
+- int memory_remaining_percentage is 'unsigned int' type
+- last_uplink_command is 'unsigned long' type
+- last_uplink_command_parameter1 is 'unsigned long' type
+- last_uplink_command_parameter2 is 'unsigned long' type
+- obc_temperature is 'int' type
+- structure_status is 'unsigned char' type
+- structure_temperature is 'int' type
+- eps_status  is 'unsigned char' type
+- eps_temperature is 'int' type
+- attitude_determination_status is 'unsigned char' type
+- communication_status is 'unsigned char' type
+- redundancy_check is 'unsigned int' type
+/*
+* Should be 32-bit machine friendly
+*/
+void get_nanosatellite_telemetry(unsigned short preamble1,
+                                 unsigned short preamble2,
+                                 unsigned short preamble3,
+                                 unsigned short preamble4,
                                  int solar_panel_voltage,
                                  int battery_charge,
                                  int battery_temperature,
@@ -17,33 +46,10 @@ void get_nanosatellite_telemetry(long preamble,
                                  int structure_temperature,
                                  char eps_status,
                                  int eps_temperature,
-                                 char attitude_determination_status 
+                                 char attitude_determination_status, 
                                  int redundancy_check ){//get_nanosatellite_telemetry
 long_size=sizeof(long);
 int_size=sizeof(int);
-unsigned int first; unsigned int second; unsigned int third; unsigned int fourth; unsigned int fifth; unsigned int sixth;
-first = ( (solar_panel)|
-          (battery_charge)|
-          (battery_temperature)|
-           );
-second= position_latitude;
-third = position_longitude;
-fourth = (  (position_altitude)|
-          (system_time)|
-          (payload_status)|
-  (payload_temperature)|
-          (obc_status)|
-          (obc_temperature)|
-          (structure_status) );
-fifth = ( (structure_temperature)|
-          (eps_status)|
-          (eps_temperature)|
-          (attitude_determination_status) );
-sixth = ( (structure_temperature)|
-          (eps_status)|
-          (eps_temperature)|
-          (attitude_determination_status) );
-unsigned int rslt[4];
-rslt = [first,second,third,fourth];
+
 return 0;
 }//get_nanosatellite_telemetry
