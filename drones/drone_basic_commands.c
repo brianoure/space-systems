985910af81ce6e01eb8ac5b706bb45e162806724
brianoure <61065227+brianoure@users.noreply.github.com>
2023-07-27 19:12:35 +0300

    Update and rename drone_video_processing.c to drone_basic_commands.c

---
+++ drones/drone_basic_commands.c
@@ -0,0 +1,37 @@
+/*
+*main program for controlling a quadcopter
+*/
+typedef LEFT 10;
+typedef RIGHT 33;
+typedef LOWER 45; 
+typedef RAISE 88;
+typedef FORWARD 63;
+typedef BACK 56;
+typedef SPIN 77;
+typedef TAKEOFF 28;
+typedef LAND 39;
+
+void north_rotor(long rpm);
+void west_rotor(long rpm);
+void east_rotor(long rpm);
+void south_rotor(long rpm);
+
+int run_program=1;
+
+void execute(int action){//action
+if(action==LEFT) {}//if
+if(action==RIGHT){}//if
+if(action==LOWER){}//if
+if(action==RAISE){}//if
+if(action==FORWARD){}//if
+if(action==BACK){}//if
+if(action==SPIN){}//if
+if(action==TAKEOFF){}//if
+if(action==LAND){}//if
+}//action
+
+int main(){//main
+while(run_program){//while
+
+}//while
+}//main
